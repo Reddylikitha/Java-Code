6909352f85fe5e46dfb518ae6ba6dc665e1cb2bf
Reddylikitha <reddylikitha147@gmail.com>
2024-02-02 17:29:03 +0530

    Firstprogram.java sumofn.java userinput.java

---
+++ Firstprogram.java
@@ -0,0 +1,9 @@
+/**
+ * Firstprogram
+ */
+public class Firstprogram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ userinput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * userinput
+ */
+public class userinput {
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     String a = sc.nextLine();
+    //     System.out.println("Hello "+a);
+    // }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        if("Alice".equals(a) || "Bob".equals(a)){
+            System.out.println("Hello "+a);
+        }else{
+            System.out.println("Hello Alice and Bob");
+        }
+    }
+}
